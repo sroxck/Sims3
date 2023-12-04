0e0c9b7607ce19cea222b2b8c3742f4cac651257
sroxck <642261686@qq.com>
2023-12-04 21:30:44 +0800

    feat: major stop func

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -4,27 +4,36 @@ import { extend } from "../shared";
  * @Author: sroxck
  * @Date: 2023-12-03 10:05:48
  * @LastEditors: sroxck
- * @LastEditTime: 2023-12-03 15:32:05
+ * @LastEditTime: 2023-12-04 21:20:18
  * @Description: 
  */
+let activeEffect
+let shouldTrack
 class ReactiveEffect {
   private _fn: any
   deps = [];
   active = true
-  onStop?:()=>void
+  onStop?: () => void
   public scheduler: Function | undefined
   constructor(fn, scheduler?: Function) {
     this._fn = fn
     this.scheduler = scheduler
   }
   run() {
+    // 1. 会手机依赖 shouldtrack做区分
+    if (!this.active) {
+      return this._fn()
+    }
+    shouldTrack = true
     activeEffect = this
-    return this._fn()
+    const result = this._fn()
+    shouldTrack = false
+    return result
   }
   stop() {
     if (this.active) {
       cleanupEffect(this)
-      if(this.onStop){
+      if (this.onStop) {
         this.onStop()
       }
       this.active = false
@@ -35,9 +44,11 @@ function cleanupEffect(effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect)
   });
+  effect.deps.length =0
 }
 const targetMap = new Map()
 export function track(target, key) {
+  if(!isTracking()) return
   // target - > key -> dep
   let depsMap = targetMap.get(target)
   if (!depsMap) {
@@ -49,11 +60,13 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
-  if(!activeEffect) return 
+
   dep.add(activeEffect)
   activeEffect.deps.push(dep)
 }
-
+function isTracking(){
+  return shouldTrack && activeEffect !==undefined
+}
 
 // 更新依赖,从依赖列表中找到该对象的依赖,在执行该对象当前key的所有依赖
 export function trigger(target, key) {
@@ -68,11 +81,11 @@ export function trigger(target, key) {
     }
   }
 }
-let activeEffect
+
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler)
-  
-  extend(_effect,options)
+
+  extend(_effect, options)
   _effect.run()
   const runner: any = _effect.run.bind(_effect)
   runner.effect = _effect
